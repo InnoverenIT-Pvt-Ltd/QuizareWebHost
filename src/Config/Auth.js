bf2b9a1ad6f912903e4e36732129b62f7964789f
Karisma98 <karismaswain80@gmail.com>
2023-03-30 16:32:40 -0800

    karisma

--- src/Config/Auth.js
+++ src/Config/Auth.js
@@ -1,5 +1,5 @@
-const login_url = "https://develop.tekorero.com/quizaare";
-const base_url = "https://develop.tekorero.com/quizaare";
+const login_url = "http://playerquizzera-env.eba-ha4ipsp6.eu-west-3.elasticbeanstalk.com";
+const base_url = "http://playerquizzera-env.eba-ha4ipsp6.eu-west-3.elasticbeanstalk.com";
 
 // export { base_url, login_url };
 export { base_url };
