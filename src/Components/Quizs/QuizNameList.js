62fc7327596e64e4be8e13b6a7e33e3020cccbff
Ashok2511 <ashok250411@gmail.com>
2023-04-01 15:45:16 +0530

    Ashok

---
+++ src/Components/Quizs/QuizNameList.js
@@ -0,0 +1,57 @@
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {getQuizNameList,clearQuizNameDetails} from '../../Container/Quiz/QuizAction'
+import { StyledSelect } from '../UI/Antd';
+const Option = StyledSelect.Option;
+function QuizNameList(props) {
+  useEffect(()=>{
+    props.getQuizNameList("QH4472404666122022");
+    return () => {
+       props.clearQuizNameDetails()
+     };
+   },[])
+  return (
+    <>
+    <div>
+      <StyledSelect
+      style={{
+        maxWidth:"15rem"
+      }}
+      name="quizName"
+      placeholder="Select Quiz Name"
+      selectedValue={props.item}
+      onChange={itemValue =>
+         props.handleGetQuizData(itemValue)
+      }
+      > 
+      {props.quizNameList.map((item, key) => (
+        <Option 
+       // label={`${item.quizName || ''}`}
+        value={`${item.quizId || ''}`}
+        key={key}
+        
+        >{`${item.quizName || ''}`}</Option>
+        
+        ))}
+        </StyledSelect>
+      
+    </div>
+    </>
+  )
+}
+
+const mapStateToProps = ({quiz}) => ({  
+  quizNameList:quiz.quizNameList,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {    
+       getQuizNameList,        
+       clearQuizNameDetails
+    },
+    dispatch,
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizNameList);
--- src/Container/Quiz/Child/HostQuizes/FinalizeQuiz.js
+++ src/Container/Quiz/Child/HostQuizes/FinalizeQuiz.js
@@ -6,91 +6,97 @@ import { Button, Card } from "antd";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import MainHeader from "../../../../Components/Mainheader";
+import { Form, Formik } from "formik";
 
 function FinalizeQuiz(props) {
-    const history = useHistory();
- 
+  const history = useHistory();
+
   function handleCallBack(data) {
-    history.push(`/create`);
+    history.push(`/updateQuizName`);
   }
   useEffect(() => {
     props.getFinalizeQuiz(props.showQuiz && props.showQuiz.quizId);
   }, []);
   return (
     <>
-    <MainHeader/>
-     <div className="bg-white rounded-rounded2.8 mt-3 ">
-           
-           <div class="shadow-2xl border-solid  p-1 max-sm:m-0 h-h34 rounded-rounded2.8 md:m-auto">
-      <h1>{`${props.finalizeQuiz.quizName || ""}`}</h1>
-     <div class="mt-20">
-     <div style={{boxShadow: "0.01rem 0.01rem 0.12rem 0.01rem"}} className="bg-white rounded-2xl  w-full flex justify-center ">
-           
-           <div class="shadow-2xl border-solid w-full  p-1 max-sm:m-0 h-28 rounded-2xl md:m-auto">
-        <h3>Created:</h3>
-        <h3>Questions:  {`${props.finalizeQuiz.noOfQuestions || ""}`}</h3>
-        <h3>Categories:</h3>
-        </div>
-        </div>
-      </div>
-      <div class="flex justify-center mt-8">
-    
-                            <h2 class="text-2xl font-bold" >Select quiz rules</h2>
-                        </div>
-                        <div >
-                        <div style={{boxShadow: "0.01rem 0.01rem 0.12rem 0.01rem"}} className="bg-white rounded-2xl  w-full flex justify-center mt-2 ">
-           
-           <div class="shadow-2xl border-solid w-full  p-1 max-sm:m-0 h-28 rounded-2xl md:m-auto">
-                                <div class="flex flex-row">
-                                    <h2 class="text-base font-bold" >
-                                        Question response time:
-                                    </h2>
-                                    <h2 class="text-base font-bold" >
-                                        {`${props.finalizeQuiz.duration || ""}`} sec
-                                    </h2>
-                                </div>
-                                <div class="flex flex-row">
-                                <h2 class="text-base font-bold" >
-                                        Scoring system:
-                                    </h2>
-                                    <h2 class="text-base font-bold" >
-                                        Standard
-                                    </h2>
-                                </div>                           
-                            </div>
-                            </div>
-                            </div>
-                            <div class="flex flex-row mt-8 justify-between">
-                            <Link to="/create">
-                            <Button
-                              type="primary"
-                              style={{ width:"11rem",backgroundColor:"white"}}
-                              onClick={() => props.deleteHostQuiz(props.showQuiz&&props.showQuiz.quizId,handleCallBack())}
-                            ><h3>Delete This Quiz</h3></Button>
-                            </Link>
-                         <Link to="/create">
-                           <Button
-                           style={{ width:"11rem",backgroundColor:"white"}}
-                              type="primary"
-                             
-                            //   onClick={() => props.navigation.navigate('Quiz Invite')}
-                            ><h3>Edit This Quiz</h3></Button>
-                            </Link> 
-                            
-                        </div>
-                        <div class="mt-3">
-                        <Link to="/hostquiz">
-                        <Button
-                              type="primary"
-                              style={{backgroundColor:"white"}}
-                              onClick={() => props.hostQuiz()}
-                            ><h3>Host This Quiz</h3></Button>
-                            </Link>
-                            </div>
+      <MainHeader />
+      <Formik>
+        <Form class=" max-sm:w-5/6 mt-8 m-auto md:mt-12  w-1/5  h-h50  ">
+          <div className="bg-white rounded-rounded2.8 mt-3 ">
+
+            <div class="shadow-2xl border-solid  p-1 max-sm:m-0 h-h34 rounded-rounded2.8 md:m-auto">
+              <h1>{`${props.finalizeQuiz.quizName || ""}`}</h1>
+              <div class="mt-20">
+                <div style={{ boxShadow: "0.01rem 0.01rem 0.12rem 0.01rem" }} className="bg-white rounded-2xl  w-full flex justify-center ">
+
+                  <div class="shadow-2xl border-solid w-full  p-1 max-sm:m-0 h-28 rounded-2xl md:m-auto">
+                    <h3>Created:</h3>
+                    <h3>Questions:  {`${props.finalizeQuiz.noOfQuestions || ""}`}</h3>
+                    <h3>Categories:</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="flex justify-center mt-8">
+
+                <h2 class="text-2xl font-bold" >Select quiz rules</h2>
+              </div>
+              <div >
+                <div style={{ boxShadow: "0.01rem 0.01rem 0.12rem 0.01rem" }} className="bg-white rounded-2xl  w-full flex justify-center mt-2 ">
+
+                  <div class="shadow-2xl border-solid w-full  p-1 max-sm:m-0 h-28 rounded-2xl md:m-auto">
+                    <div class="flex flex-row">
+                      <h2 class="text-base font-bold" >
+                        Question response time:
+                      </h2>
+                      <h2 class="text-base font-bold" >
+                        {`${props.finalizeQuiz.duration || ""}`} sec
+                      </h2>
+                    </div>
+                    <div class="flex flex-row">
+                      <h2 class="text-base font-bold" >
+                        Scoring system:
+                      </h2>
+                      <h2 class="text-base font-bold" >
+                        Standard
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-row mt-8 justify-between">
+                <Link to="/create">
+                  <Button
+                    type="primary"
+                    style={{ width: "9rem", backgroundColor: "white" }}
+                    onClick={() => props.deleteHostQuiz(props.showQuiz && props.showQuiz.quizId, handleCallBack())}
+                  ><h3>Delete This Quiz</h3></Button>
+                </Link>
+                <Link to="/updateQuizName">
+                  <Button
+                    style={{ width: "9rem", backgroundColor: "white" }}
+                    type="primary"
+
+                  //   onClick={() => props.navigation.navigate('Quiz Invite')}
+                  ><h3>Edit This Quiz</h3></Button>
+                </Link>
+
+              </div>
+              <div class="mt-3">
+                <Link to="/hostquiz">
+                  <Button
+                    type="primary"
+                    style={{ backgroundColor: "white" }}
+                    onClick={() => props.hostQuiz(props.showQuiz.quizId)}
+                  ><h3>Host This Quiz</h3></Button>
+                </Link>
+              </div>
 
 
-</div>
-      </div>
+            </div>
+          </div>
+        </Form>
+      </Formik>
+
     </>
   );
 }
