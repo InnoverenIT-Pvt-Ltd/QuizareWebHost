98b3c334a85de01a4223187b2ab32972ef22d0e7
silpa82603 <82430661+silpa82603@users.noreply.github.com>
2023-07-12 15:50:56 +0530

    12-07-23 new changes

--- src/Config/Auth.js
+++ src/Config/Auth.js
@@ -1,5 +1,5 @@
-const login_url = "https://playerquizzera-env.eba-ha4ipsp6.eu-west-3.elasticbeanstalk.com";
-const base_url = "https://playerquizzera-env.eba-ha4ipsp6.eu-west-3.elasticbeanstalk.com";
+const login_url = "https://develop.quizledge.no";
+const base_url = "https://develop.quizledge.no";
 
 // export { base_url, login_url };
 export { base_url, login_url };
