76e861c64dbd233d451de2f3a172cdc990c2d61a
Dibya391 <dibyaranjan828@gmail.com>
2024-12-10 09:34:43 +0530

    drb3

--- src/Config/Auth.js
+++ src/Config/Auth.js
@@ -1,8 +1,8 @@
-// const login_url = "https://develop.quizledge.no";
-// const base_url = "https://develop.quizledge.no";
+const login_url = "https://QuizledgeBackend.quizprompter.com";
+const base_url = "https://QuizledgeBackend.quizprompter.com";
 
-const login_url = "https://QuizledgeBackend.quizledge.no";
-const base_url = "https://QuizledgeBackend.quizledge.no";
+// const login_url = "https://QuizledgeBackend.quizledge.no";
+// const base_url = "https://QuizledgeBackend.quizledge.no";
 
 // const base_url2="https://13.127.118.114";
 const base_url2="https://openapidev.quizledge.no";
